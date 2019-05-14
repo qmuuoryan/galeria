ff945a709ef194a980fd05f86d2300f725f2c1a8
ryan <you@example.com>
2019-05-14 12:00:27 +0300

    solved a http error

--- galeria/settings.py
+++ galeria/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap3',
+    'imagegaleria',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- imagegaleria/views.py
+++ imagegaleria/views.py
@@ -1,9 +1,12 @@
+from django.http  import HttpResponse,Http404
 from django.http  import HttpResponse
 import datetime as dt
+from django.shortcuts import render,redirect
+
 from django.shortcuts import render
 
 
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse(request, 'welcome to my gallery')
+    return render(request, 'welcome.html')
