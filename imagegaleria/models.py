f6b888e75cccde139faacf6b84a593ff7001b370
ryan <you@example.com>
2019-05-14 15:43:35 +0300

    created an image model class

--- imagegaleria/models.py
+++ imagegaleria/models.py
@@ -20,3 +20,48 @@ class Category(models.Model):
 
     def __str__(self):
             return self.category_name
+
+
+class Image(models.Model):
+    image_name = models.CharField(max_length = 70)
+    description = models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category )
+    photo = models.ImageField(upload_to = 'images/')
+
+    def __str__(self):
+        return self.image_name
+    class Meta:
+        ordering = ['image_name']
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+        
+    @classmethod
+    def get_image_by_id(cls, image_id):
+        image = cls.objects.get(pk=id)
+        return cls.objects.get(id=image_id)
+
+    @classmethod
+    def update_image(cls,id,name,description,location,category):
+        image = cls.objects.get(pk=image_id)
+        image = cls(name=name,description=description,location=location,category=category)
+        image.save()
+
+    @classmethod
+    def filter_by_location(cls, location):
+        images = cls.objects.filter(location=location)
+        return images
+
+    @classmethod
+    def all_images(cls):
+        images = cls.objects.all()
+        return images
+    
+    @classmethod
+    def search_by_category(cls,search_term):
+        result_search = cls.objects.filter(category_name__icontains=search_term)
+        return result_search    
--- imagegaleria/tests.py
+++ imagegaleria/tests.py
@@ -1,3 +1,4 @@
 from django.test import TestCase
+from .models import Location,Category,Image
 
 # Create your tests here.
