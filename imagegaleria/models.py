a7ff9825cb28871d76175a929b3e994c65754e2f
ryan <you@example.com>
2019-05-14 15:06:06 +0300

    created a location class

--- imagegaleria/models.py
+++ imagegaleria/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 # Create your models here.
-def Location(models.Model):
+class Location(models.Model):
     location_name = models.CharField(max_length =30)
 
-     def __str__(self):
+    def __str__(self):
             return self.location
-    
+
+
+class Category(model.Model):
