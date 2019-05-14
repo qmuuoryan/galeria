fc530f7ba876fe12ae9e40db864b31d5e3604cb9
ryan <you@example.com>
2019-05-14 15:19:43 +0300

    created the category class model

--- imagegaleria/models.py
+++ imagegaleria/models.py
@@ -8,4 +8,15 @@ class Location(models.Model):
             return self.location
 
 
-class Category(model.Model):
+class Category(models.Model):
+    category_name = models.CharField(max_length =30)
+
+
+    def save_category(self):
+            self.save()
+
+    def delete_category(self):
+            self.delete()
+
+    def __str__(self):
+            return self.category_name
