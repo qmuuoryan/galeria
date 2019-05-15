e9951e7ac646df4e490e88612264d66533aa3219
ryan <you@example.com>
2019-05-15 12:28:03 +0300

    updated the models and navbar

--- imagegaleria/models.py
+++ imagegaleria/models.py
@@ -4,8 +4,17 @@ from django.db import models
 class Location(models.Model):
     location_name = models.CharField(max_length =30)
 
+
+    def save_Location(self):
+            self.save()
+
+    # def delete_Location(self):
+    #         self.delete()
+
+
     def __str__(self):
             return self.location_name
+
 
 class Category(models.Model):
     category_name = models.CharField(max_length =30)
