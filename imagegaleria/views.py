8cc41836e8fac34be2c3b3ef3fa9f7f0579d0345
ryan <you@example.com>
2019-05-16 01:02:07 +0300

    updated my views

--- imagegaleria/views.py
+++ imagegaleria/views.py
@@ -1,12 +1,32 @@
 from django.http  import HttpResponse,Http404
-from django.http  import HttpResponse
-import datetime as dt
 from django.shortcuts import render,redirect
-
-from django.shortcuts import render
-
+from .models import Location,Category,Image
 
 
 # Create your views here.
 def welcome(request):
-    return render(request, 'welcome.html')
+    locations=Location.objects.all()
+    categorys=Category.objects.all()
+    # images=Image.retrieve_all()
+    return render (request, 'welcome.html',{'locations':locations,'categorys':categorys,'images':images})
+
+def app_location(request):
+    locations=Location.objects.all()
+    return render (request,'welcome.html')
+
+def app_category(request):
+    categorys=Category.objects.all()
+    return render (request,'welcome.html')
+
+def search_category(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"Results for: {search_term}"
+
+        return render(request, 'all-images/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term."
+        return render(request, 'all-images/search.html',{"message":message})
+
