201e3cb559c8814234baba1569e7bbc5a6dfe76e
ryan <you@example.com>
2019-05-15 12:27:04 +0300

    created my tests

--- imagegaleria/tests.py
+++ imagegaleria/tests.py
@@ -2,4 +2,39 @@ from django.test import TestCase
 from .models import Location,Category,Image
 
 # Create your tests here.
- 
+# category test class
+class Category_test(TestCase):
+    def setUp(self):
+        self.Art=Category(id=1,category_name="Art")
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Art,Category))
+
+    def test_save_category(self):
+        self.Art.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category)>0)
+
+    def test_delete_category(self):
+        self.Art.delete_category()
+        category= Category.objects.all()
+        self.assertTrue(len(category) < 1)
+
+# location test class
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.prestige= Location(id=1 ,location_name= 'prestige')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.prestige,Location))
+    
+    def test_save_method(self):
+        self.prestige.save_Location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    # def test_delete_Location(self):
+    #     self.prestige.delete_Location()
+    #     locations= Location.objects.all()
+    #     self.assertTrue(len(location) < 1)    
+               
